18609abd8ab4d8fd61babc65c5afef65ee5f46d6
cybergames1 <jtliu.tju@gmail.com>
2015-09-26 21:50:26 +0800

    Initial Commit

---
+++ HomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeWork
+//
+//  Created by jianting on 15/9/26.
+//  Copyright © 2015年 jianting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HomeWork
+//
+//  Created by jianting on 15/9/26.
+//  Copyright © 2015年 jianting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
